033fc837f8a711e76148b11d270f940c0364b1a8
AntonZenko <zenko_anton@ukr.net>
2022-05-24 18:16:29 +0300

    gallery and image added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainerRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMurkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainerRef.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryMurkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+        </div>`;
+    })
+    .join('');
+}
+
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+
+  const galleryItem = e.target.classList.contains('gallery__image');
+  if (!galleryItem) {
+    return;
+  }
+}
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: `alt`, captionDelay: `250` });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash/throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
