8a083586fdff399e78a379264065a5cc03765a2e
AntonZenko <zenko_anton@ukr.net>
2022-05-27 02:05:56 +0300

    bugfix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,36 @@
 import throttle from 'lodash/throttle';
-import {storage} from './storage.js';
+import storage from './storage.js';
 
-const { saveUserData, getUserData } = storage;
+const { saveUserData, getUserData, removeUserData } = storage;
 const formRef = document.querySelector('.feedback-form');
-
 
 const STORAGE_KEY = 'feedback-form-state';
 
-fillUserForm();
-
-const formData = {
-  email: formRef.email.value,
-  message: formRef.message.value,
-};
+const formData = getUserData(STORAGE_KEY) || {};
 
 formRef.addEventListener('submit', handleSubmit);
 formRef.addEventListener('input', throttle(handleInput, 500));
 
+fillUserForm();
 
 function handleInput(e) {
-  formData.[e.target.name] = e.target.value;
+  formData[e.target.name] = e.target.value;
   saveUserData(STORAGE_KEY, formData);
-  // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function handleSubmit(e) {
   e.preventDefault();
-  const userData = {
-  email: e.currentTarget.email.value,
-  message: e.currentTarget.message.value,
-  };
-  console.log(userData);
-  localStorage.removeItem(STORAGE_KEY);
-  formRef.reset();
-
+  console.log(formData);
+  e.target.reset();
+  removeUserData(STORAGE_KEY);
 }
 
 function fillUserForm() {
   const savedData = getUserData(STORAGE_KEY);
   if (!savedData) return;
-  if(savedData.email) {
+
+  if (savedData) {
     formRef.email.value = savedData.email;
-  }
-  if(savedData.message) {
     formRef.message.value = savedData.message;
   }
 }
